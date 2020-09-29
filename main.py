24c7804fc49d2e26507b28be732f7654d333dabb
genyu <736640862@qq.com>
2020-09-29 12:52:03 +0800

    update

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ train, valid, test = divide_dataset(get_train_data())
 tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 train_padded = tokenizer(train_sentences, padding=True, truncation=True, is_split_into_words=True, return_tensors='pt')
 valid_padded = tokenizer(valid_sentences, padding=True, truncation=True, is_split_into_words=True, return_tensors='pt')
+
+movemove
