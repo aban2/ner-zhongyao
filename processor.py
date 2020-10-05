7b3d5f5dc16e46c74fa2269ff313ab6d7e413003
genyu <736640862@qq.com>
2020-10-05 09:49:10 +0800

    update

--- processor.py
+++ processor.py
@@ -7,7 +7,6 @@ from torch.utils.data import TensorDataset, DataLoader, RandomSampler, Sequentia
 from transformers import BertTokenizer, BertConfig, BertForTokenClassification, AdamW, get_linear_schedule_with_warmup
 
 class Processor:
-
 	def __init__(self, train=None):
 		self.tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 		if train != None:
@@ -21,6 +20,7 @@ class Processor:
 			self.device = torch.device('cpu')
 			name = 'Cpu'
 		print('Running On', name)
+		self.model = BertForTokenClassification.from_pretrained("bert-base-chinese", num_labels=len(self.label2id)).to(self.device)
 
 	def data2loader(self, data, mode, batch_size):
 		padded_data, padded_labels, followed = padding(data, self.tokenizer, self.label2id)
@@ -34,17 +34,14 @@ class Processor:
 		dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
 		return dataloader, followed
 
-	def train(self, train, num_epoches, max_grad_norm, batch_size, save_epoch):
+	def train(self, train, valid, num_epoches, batch_size, save_epoch, max_grad_norm=1.0):
 		# get dataloader
 		train_dataloader, _ = self.data2loader(train, mode='train', batch_size=batch_size)
-
-		# model
-		model = BertForTokenClassification.from_pretrained("bert-base-chinese", num_labels=len(self.label2id)).to(self.device)
 
 		# optimizer and scheduler
 		FULL_FINETUNING = True
 		if FULL_FINETUNING:
-		    param_optimizer = list(model.named_parameters())
+		    param_optimizer = list(self.model.named_parameters())
 		    no_decay = ['bias', 'gamma', 'beta']
 		    optimizer_grouped_parameters = [
 		        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
@@ -71,7 +68,7 @@ class Processor:
 
 		# training
 		for i in range(num_epoches):
-			model.train()
+			self.model.train()
 
 			losses = 0
 			for idx, batch_data in enumerate(train_dataloader):
@@ -79,15 +76,15 @@ class Processor:
 				batch_data = tuple(i.to(self.device) for i in batch_data)
 				ids, masks, labels = batch_data
 
-				model.zero_grad()
-				loss, _ = model(ids, attention_mask=masks, labels=labels)
+				self.model.zero_grad()
+				loss, _ = self.model(ids, attention_mask=masks, labels=labels)
 
 				# process loss
 				loss.backward()
 				losses += loss.item()
 
 				# tackle exploding gradients
-				torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=max_grad_norm)
+				torch.nn.utils.clip_grad_norm_(parameters=self.model.parameters(), max_norm=max_grad_norm)
 
 				optimizer.step()
 				scheduler.step()
@@ -95,18 +92,23 @@ class Processor:
 				# if (idx+1) % 100 == 0:
 				# 	print('batch', idx+1, 'loss', losses/(idx+1))
 
-			print('Epoch', i, losses/len(train_dataloader))
+			F1, loss = self.evaluate(valid)
+			print('Epoch', i, losses/len(train_dataloader), loss, 'F1', F1)
 
 			if (i+1) % save_epoch == 0:
-				torch.save(model, 'Mod' + str(i+1))
+				torch.save(model, 'models/Mod' + str(i+1))
 
 		return model
 
-	def evaluate(self, epoch, valid):
+	def evaluate(self, valid, epoch=None):
 		# get dataloader
-		dataloader, followed = self.data2loader(valid, 'valid', batch_size=512)
+		dataloader, followed = self.data2loader(valid, 'valid', batch_size=1024)
 
-		model = torch.load('models/Mod' + epoch)
+		if epoch != None:
+			model = torch.load('models/Mod' + epoch)
+		else:
+			model = self.model
+
 		model.eval()
 		# print(model)
 
@@ -115,69 +117,70 @@ class Processor:
 			recalls, accs = [], []
 
 			# process all data in one batch
+			F1s = []
 			for idx, batch_data in enumerate(dataloader):
 				batch_data = tuple(i.to(self.device) for i in batch_data)
 				ids, masks, labels = batch_data
 				loss, logits = model(ids, attention_mask=masks, labels=labels) # loss and logits
-				print(loss)
 
 				results = torch.argmax(logits, dim=2)
 				results = results*masks
 
+
 				for idx, result in enumerate(results):
 
 					lenth = torch.sum(masks[idx]).item()
-					print(labels[idx][0:lenth+1])
-					print()
-					print(result[0:lenth+1])
-					print()
-					channel_true, channel_pred = -1, -1
-					following, correct, total_preds, total_true = 0, 0, 0, 0
+					# print(labels[idx][0:lenth+1])
+					# print()
+					# print(result[0:lenth+1])
+					# print()
 
+					channel_pred, channel_true = -1, -1
+					following, correct, total_pred, total_true = -1, 0, 0, 0
 					for jdx, (label_pred, label_true) in enumerate(zip(result, labels[idx])):
 						label_true, label_pred = label_true.item(), label_pred.item()
 
-						# process channel_true
-						if channel_true > 1:
-							if label_true != channel_true:
-								if label_true > 1:
-									total_true += 1
-								if following == 1 and label_pred != channel_pred:
-									correct += 1
-								following = 0
+						# process total true
+						if label_true != channel_true:
+							if following == 1 and label_pred != channel_pred:
+								correct += 1
+								# print(channel_pred, channel_true)
+							following = 0
+
+						if label_true & 1 and label_true > 1:
+							channel_true = label_true - 1
+							total_true += 1
 						else:
-							if label_true > 1:
-								total_true += 1
-								following = 0
+							channel_true = label_true
 
-						# process channel_pred
-						if channel_pred > 1:
-							if channel_pred != label_pred:
-								if label_pred > 1:
-									total_preds += 1
-									if following == 0:
-										following = 1
-
-							if following == 1:
-								if label_pred != label_true:
-									following = -1
+						# process total preds
+						if label_pred & 1 and label_pred > 1:
+							total_pred += 1
+							channel_pred = label_pred - 1
+							if following == 0 and label_pred == label_true:
+								following = 1
 						else:
-							if label_pred > 1:
-								total_preds += 1
-								if following == 0:
-									if label_pred == label_true:
-										following = 1
-									else:
-										following = -1
+							channel_pred = label_pred
 
-						channel_true = label_true
-						channel_pred = label_pred
+						if following == 1 and label_pred != label_true:
+							following = -1
 
 						if label_true == 0:
 							break
 
-					print(correct, total_preds, total_true)
+					if total_pred == 0:
+						precision = 0
+					else:
+						precision = correct / total_pred
+					if total_true == 0:
+						recall = 0
+					else:
+						recall = correct / total_true
+					if precision + recall == 0:
+						F1 = 0
+					else:
+						F1 = 2*(precision*recall) / (precision+recall)
+					F1s.append(F1)
 
-					
-					if idx == 1:
-						break
+			# print('Evaluation: F1', np.mean(F1s), 'loss', loss)
+			return F1, loss
