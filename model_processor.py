6ef7f65c238fc5daae66800ff65921ec0666db74
genyu <736640862@qq.com>
2020-10-13 22:18:58 +0800

    update

--- model_processor.py
+++ model_processor.py
@@ -36,29 +36,23 @@ class Processor:
 		# get dataloader
 		train_dataloader, _ = self.data2loader(self.args['train'], mode='train', batch_size=self.args['batch_size'])
 		# optimizer and scheduler
-		FULL_FINETUNING = True
-		if FULL_FINETUNING:
-		    param_optimizer = list(self.model.named_parameters())
-		    other_parameters = [(n, p) for n, p in param_optimizer if 'crf' not in n]
-		    no_decay = ['bias', 'gamma', 'beta']
-		    optimizer_grouped_parameters = [
-		        {'params': [p for n, p in other_parameters if not any(nd in n for nd in no_decay)],
-		         'weight_decay_rate': 0.01},
-		        {'params': [p for n, p in other_parameters if any(nd in n for nd in no_decay)],
- 				'weight_decay_rate': 0.0},
- 				{'params':[p for n, p in param_optimizer if 'crf' in n],
- 				 'lr':3e-2}
-		    ]
-		else:
-			param_optimizer = list(self.model.classifier.named_parameters())
-			optimizer_grouped_parameters = [{"params": [p for n, p in param_optimizer]}]
 
-		self.optimizer = AdamW(
+	    param_optimizer = list(self.model.named_parameters())
+	    other_parameters = [(n, p) for n, p in param_optimizer if 'crf' not in n]
+	    no_decay = ['bias', 'gamma', 'beta']
+	    optimizer_grouped_parameters = [
+	        {'params': [p for n, p in other_parameters if not any(nd in n for nd in no_decay)],
+	         'weight_decay_rate': 0.01},
+	        {'params': [p for n, p in other_parameters if any(nd in n for nd in no_decay)],
+				'weight_decay_rate': 0.0},
+			{'params':[p for n, p in param_optimizer if 'crf' in n], 'lr':3e-2} ]
+
+		optimizer = AdamW(
 			optimizer_grouped_parameters, lr=3e-5, eps=1e-8
 		)
 
 		if self.args['load_model'] > 0:
-			self.optimizer.load_state_dict(torch.load('models/Opt' + str(self.args['load_model'])))
+			optimizer.load_state_dict(torch.load('models/Opt' + str(self.args['load_model'])))
 			print('load optimizer success')
 		
 		total_steps = 1000#len(train_dataloader) * num_epoches
@@ -67,7 +61,7 @@ class Processor:
 		else:
 			last_epoch = self.args['load_model']
 		scheduler = get_linear_schedule_with_warmup(
-		    self.optimizer,
+		    optimizer,
 		    num_warmup_steps=0,
 		    num_training_steps=total_steps,
 		    last_epoch=last_epoch
